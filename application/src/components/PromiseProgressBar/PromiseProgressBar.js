414991b2b310e22e96957c9d8a0575ae0ab6af44
AlexKovalchuk <alexkovalchuk1991@gmail.com>
2018-07-23 22:12:09 +0300

    create helper

---
+++ application/src/components/PromiseProgressBar/PromiseProgressBar.js
@@ -0,0 +1,20 @@
+/**
+ * Created by alex on 7/23/18.
+ */
+import React, {Component} from 'react';
+import './PromiseProgressBar.css';
+import helper from '../../helpers/PromiseProgressBar';
+
+class PromiseProgressBar extends Component {
+    render() {
+        console.log('progress bar render');
+        helper.progressBar();
+        return(
+            <div>
+                here will be my progress bar
+            </div>
+        )
+    }
+}
+
+export default PromiseProgressBar;
---
+++ application/src/helpers/PromiseProgressBar.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alex on 7/23/18.
+ */
+
+const progressBar = () => {
+    console.log('helper');
+};
+
+export default {progressBar};
