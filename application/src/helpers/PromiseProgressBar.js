c53f7138924fb7973aa1ad6b65788ce88df4734b
AlexKovalchuk <alexkovalchuk1991@gmail.com>
2018-08-27 22:49:08 +0300

    in progress

--- application/src/helpers/PromiseProgressBar.js
+++ application/src/helpers/PromiseProgressBar.js
@@ -2,29 +2,60 @@
  * Created by alex on 7/23/18.
  */
 
-const promiseIndicator = (promiseArray) => {
-    let chain = Promise.resolve();
-    let results = [];
-    let progressChecker = 0;
+class PromiseProgressBar {
+    constructor(collBackFunction){
+        this._totalCount = 0;
+        this._doneSuccess = 0;
+        this._doneError = 0;
+        this._promiseList = new Map();
+    }
 
-    // console.log('helper', promiseArray);
-    // начало цепочки
-    promiseArray.forEach((promise, index) => {
-        chain = chain
-            .then(() => promise)
-            .then(result => {
-                progressChecker++;
-                console.log('result', result, 'done =', progressChecker);
-                results.push(result);
-            });
-    });
+    promiseCollback = promise => {
 
-// в конце — выводим результаты
-    chain.then(() => {
-        console.log('results total:', results);
-    });
-};
+    };
+
+    resolvePromises = promiseArray => {
+        for (let promise of promiseArray) {
+            this._promiseList.set(promise, false);
+        }
+        this._totalCount = 0;
+        this._doneSuccess = 0;
+        this._doneError = 0;
+        for (let promise of this._promiseList) {
+            promise
+                .then(
+                    resolve => this.promiseCollback(promise),
+                    error => this.promiseCollback(promise,error)
+                )
+        }
+    }
+
+}
 
 
-
-export default promiseIndicator;
+// const promiseIndicator = (promiseArray) => {
+//     let chain = Promise.resolve();
+//     let results = [];
+//     let progressChecker = 0;
+//
+//     // console.log('helper', promiseArray);
+//     // начало цепочки
+//     promiseArray.forEach((promise, index) => {
+//         chain = chain
+//             .then(() => promise)
+//             .then(result => {
+//                 progressChecker++;
+//                 console.log('result', result, 'done =', progressChecker);
+//                 results.push(result);
+//             });
+//     });
+//
+// // в конце — выводим результаты
+//     chain.then(() => {
+//         console.log('results total:', results);
+//     });
+// };
+//
+//
+//
+// export default promiseIndicator;
